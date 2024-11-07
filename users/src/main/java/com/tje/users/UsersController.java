b48a30589a51d32a14c6b245037d3323fbabd0d7
Dominika Jelińska <dominikajelinska82@gmail.com>
2024-11-07 15:22:53 +0100

    Dodanie obliczania dni od rejestracji użytkownika

--- users/src/main/java/com/tje/users/UsersController.java
+++ users/src/main/java/com/tje/users/UsersController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
-import java.util.Locale;
 
 @Controller
 public class UsersController {
@@ -20,8 +20,15 @@ public class UsersController {
         String startDateString = "20/05/2007 07:32";
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         Date startDate = df.parse(startDateString);
+
+        LocalDateTime registrationDateTime = startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        long daysSinceRegistration = Duration.between(registrationDateTime, currentDateTime).toDays();
+
         User user = new User(2, "Artur", 34, User.UserType.ADMIN, startDate);
         model.addAttribute("user", user);
+        model.addAttribute("daysSinceRegistration", daysSinceRegistration);
+
         return "home";
     }
 }
